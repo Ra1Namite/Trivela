64b7037b8a94dd31388f559a166dbb51ff4459ba
Sanjaya Rai <rainamite@gmail.com>
2024-04-14 19:09:24 +0545

    Get query by ID added and also get all query added

--- test_orm.py
+++ test_orm.py
@@ -57,3 +57,71 @@ def test_save_author_instances(db, Author):
     rooney = Author(name="Wayne", age=30)
     db.save(rooney)
     assert rooney.id == 4
+
+
+def test_query_all_authors(db, Author):
+    db.create(Author)
+    david = Author(name="David", age=33)
+    ryan = Author(name="Ryan", age=24)
+    db.save(david)
+    db.save(ryan)
+    authors = db.all(Author)
+    assert Author._get_select_all_sql() == (
+        "SELECT id, age, name FROM author;",
+        ["id", "age", "name"],
+    )
+    assert len(authors) == 2
+    assert type(authors[0]) == Author
+    assert {a.age for a in authors} == {24, 33}
+    assert {a.name for a in authors} == {"David", "Ryan"}
+
+
+def test_get_author(db, Author):
+    db.create(Author)
+    david = Author(name="David", age=22)
+    db.save(david)
+    david_from_db = db.get(Author, id=1)
+    assert Author._get_select_where_sql(id=1) == (
+        "SELECT id, age, name FROM author WHERE id = ?;",
+        ["id", "age", "name"],
+        [1],
+    )
+    assert type(david_from_db) == Author
+    assert david_from_db.age == 22
+    assert david_from_db.name == "David"
+    assert david_from_db.id == 1
+
+
+def test_get_book(db, Author, Book):
+    db.create(Author)
+    db.create(Book)
+    david = Author(name="David", age=22)
+    messi = Author(name="Messi", age=33)
+    book = Book(title="Hello World", published=False, author=david)
+    book1 = Book(title="Trivela Book", published=True, author=messi)
+    db.save(david)
+    db.save(messi)
+    db.save(book)
+    db.save(book1)
+    book_from_db = db.get(Book, 2)
+    assert book_from_db.title == "Trivela Book"
+    assert book_from_db.author.name == "Messi"
+    assert book_from_db.author.id == 2
+
+
+def test_query_all_books(db, Author, Book):
+    db.create(Author)
+    db.create(Book)
+    david = Author(name="David", age=22)
+    messi = Author(name="Messi", age=33)
+    book = Book(title="Hello World", published=False, author=david)
+    book1 = Book(title="Trivela Book", published=True, author=messi)
+    db.save(david)
+    db.save(messi)
+    db.save(book)
+    db.save(book1)
+
+    books = db.all(Book)
+
+    assert len(books) == 2
+    assert books[1].author.name == "Messi"
--- trivela/orm.py
+++ trivela/orm.py
@@ -16,9 +16,37 @@ class Database:
 
     def save(self, instance):
         sql, values = instance._get_insert_sql()
-        cursor =self.conn.execute(sql, values)
+        cursor = self.conn.execute(sql, values)
         instance._data["id"] = cursor.lastrowid
         self.conn.commit()
+
+    def all(self, table):
+        sql, fields = table._get_select_all_sql()
+        result = []
+        for row in self.conn.execute(sql).fetchall():
+            instance = table()
+            for field, value in zip(fields, row):
+                if field.endswith("_id"):
+                    field = field[:-3]
+                    fk = getattr(table, field)
+                    value = self.get(fk.table, id=value)
+                setattr(instance, field, value)
+            result.append(instance)
+        return result
+
+    def get(self, table, id):
+        sql, fields, params = table._get_select_where_sql(id=id)
+        row = self.conn.execute(sql, params).fetchone()
+        if row is None:
+            raise Exception(f"{table.__name__} instance with id {id} does not exist")
+        instance = table()
+        for field, value in zip(fields, row):
+            if field.endswith("_id"):
+                field = field[:-3]
+                fk = getattr(table, field)
+                value = self.get(fk.table, id=value)
+            setattr(instance, field, value)
+        return instance
 
 
 class Table:
@@ -50,6 +78,33 @@ class Table:
         return sql, values
 
     @classmethod
+    def _get_select_where_sql(cls, id):
+        SELECT_WHERE_SQL = "SELECT {fields} FROM {name} WHERE id = ?;"
+        fields = ["id"]
+        for name, field in inspect.getmembers(cls):
+            if isinstance(field, Column):
+                fields.append(name)
+            if isinstance(field, ForeignKey):
+                fields.append(name + "_id")
+        sql = SELECT_WHERE_SQL.format(
+            name=cls.__name__.lower(), fields=", ".join(fields)
+        )
+        params = [id]
+        return sql, fields, params
+
+    @classmethod
+    def _get_select_all_sql(cls):
+        SELECT_ALL_SQL = "SELECT {fields} FROM {name};"
+        fields = ["id"]
+        for name, field in inspect.getmembers(cls):
+            if isinstance(field, Column):
+                fields.append(name)
+            if isinstance(field, ForeignKey):
+                fields.append(name + "_id")
+        sql = SELECT_ALL_SQL.format(name=cls.__name__.lower(), fields=", ".join(fields))
+        return sql, fields
+
+    @classmethod
     def _get_create_sql(cls):
         CREATE_TABLE_SQL = "CREATE TABLE IF NOT EXISTS {name} ({fields});"
         fields = [
@@ -71,6 +126,11 @@ class Table:
         if key in _data:
             return _data[key]
         return super().__getattribute__(key)  # avoid infinite recursion
+
+    def __setattr__(self, key, value):
+        super().__setattr__(key, value)
+        if key in self._data:
+            self._data[key] = value
 
 
 class Column:
